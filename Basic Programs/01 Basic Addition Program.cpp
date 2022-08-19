b23a7560ab7febf87a31a9e642aec9bd1b91e5f8
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-08-19 11:35:51 +0530

    Add files via upload

---
+++ Basic Programs/01 Basic Addition Program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int iNo1 = 15, iNo2 = 55, iAns = 0;
+    
+    iAns = iNo1 + iNo2;
+    
+    cout << endl << "Addition Is : " << iAns << endl ;
+    
+    getch();
+    return 0;
+}
