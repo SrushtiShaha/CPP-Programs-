caa96f3bd74cf2418570ee51fdf8c4f71d5e45a0
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-08-23 22:18:33 +0530

    Add files via upload

---
+++ Basic Programs/06 Basic Addition Program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+    int iNo1 = 0, iNo2 = 0;
+    
+    cout << endl << " \t Enter Any Two Numbers => ";
+    cin >> iNo1 >> iNo2;
+    
+    cout << endl << " \t Addition Of " << iNo1 << " And " << iNo2 << " Is => " << iNo1 + iNo2 << endl;
+    
+    getch();
+    return 0;
+}
