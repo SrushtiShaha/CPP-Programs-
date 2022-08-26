a4e7bb1edc440afe5130939a61d9042d44ff53e7
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2022-08-26 22:07:30 +0530

    Add files via upload

---
+++ Basic Programs/7 Using Class Addition Of Two Numbers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+class Sum
+{
+    public:
+          int iNo1, iNo2, iRes;  // Data Members.
+          
+          void Add()  // Behaviour/ Member Function.
+          {
+              iRes = iNo1 + iNo2;
+          }
+};
+
+int main()
+{
+    Sum Obj;
+    
+    Obj.iNo1 = 45;
+    Obj.iNo2 = 15;
+    
+    Obj.Add();
+    
+    cout << endl << " \t Addition Of " << Obj.iNo1 << " + " << Obj.iNo2 << " Is => " << Obj.iRes << endl;
+    
+    getch();
+    return 0;
+}
