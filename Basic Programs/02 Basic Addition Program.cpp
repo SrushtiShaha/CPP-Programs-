a140186f74fb868a2bc979a4449efe2003a87632
SrushtiShaha <95905998+SrushtiShaha@users.noreply.github.com>
2023-06-27 17:48:40 +0800

    CPP_Program

--- Basic Programs/02 Basic Addition Program.cpp
+++ Basic Programs/02 Basic Addition Program.cpp
@@ -10,7 +10,7 @@ int main()
     
     iAns = iNo1 + iNo2;
     
-    cout << endl << " Addition Of " << iNo1 << " And " << iNo2 << " Is => " << iAns << endl;
+    cout << endl << " Addition Of " << iNo1 << " And " << iNo2 << " Is -> " << iAns << endl;
     
     getch();
     return 0;
